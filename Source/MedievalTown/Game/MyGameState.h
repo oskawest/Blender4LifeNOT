14dfc7e0d6396c8a8f31af07f8d66988ee7758d5
Johan Rosén <johan.rosen.91@gmail.com>
2018-12-06 19:45:27 +0100

    Clean some stuff and added blueprints/classes for base functionality

---
+++ Source/MedievalTown/Game/MyGameState.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "MyGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MEDIEVALTOWN_API AMyGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
